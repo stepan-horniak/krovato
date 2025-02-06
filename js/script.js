974f32ad2ebe751136ecabf34b32f58f54985714
stepan <stepanhornjak@gmail.com>
2025-02-06 19:07:31 +0200

    header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Строгий режим
 "use strict"
-
 // Touch Screen?
 const isTouchScreen = window.matchMedia("(any-hover:none)").matches
 
@@ -36,13 +35,83 @@ function windowLoaded() {
       numberTelPopap.classList.remove("active")
       numberTelArrow.classList.remove("active")
     }
+    console.log(el)
 
     //===========header-auto-cal=================
     if (el.closest(".header-main-tell__icon")) {
       window.location.href = "tel:+380931234567"
     }
+    //=============burger-menu===================
+    if (el.closest(".header-main-burger")) {
+      const burger = document.querySelector(".header-main-burger")
+      const menu = document.querySelector(".header-main__menu-burger")
+      burger.classList.toggle("active")
+      menu.classList.toggle("active")
+    }
+    if (
+      !el.closest(".header-main-burger") &&
+      !el.closest(".header-main__menu-burger")
+    ) {
+      const burger = document.querySelector(".header-main-burger")
+      const menu = document.querySelector(".header-main__menu-burger")
+      burger.classList.remove("active")
+      menu.classList.remove("active")
+    }
 
     //================================
   }
   document.addEventListener("click", (e) => documentActions(e))
+
+  //========================
+  const mediaQuery991 = window.matchMedia("(max-width: 991.98px)")
+
+  function handleScreenChange(e) {
+    const topContainer = document.querySelector(".header-top__container")
+    const parrentTopContainer = document.querySelector(".header-top")
+
+    const bottomContainer = document.querySelector(".header-bottom__container")
+    const parrentBottomContainer = document.querySelector(".header-bottom")
+
+    const burgerMenu = document.querySelector(".header-main__menu-burger")
+
+    if (e.matches) {
+      burgerMenu.append(bottomContainer)
+      burgerMenu.append(topContainer)
+    } else {
+      parrentTopContainer.append(topContainer)
+      parrentBottomContainer.append(bottomContainer)
+    }
+  }
+  handleScreenChange(mediaQuery991)
+
+  mediaQuery991.addEventListener("change", handleScreenChange)
+  //===============================
+
+  //========================
+  const mediaQuery767 = window.matchMedia("(max-width: 767.98px)")
+
+  function handleScreenChange767(e) {
+    const mainContainer = document.querySelector(".header-main__container")
+    const parrentEl = document.querySelector(".header-bottom")
+    const headerElButton = document.querySelector(
+      ".header-main-catalog__button"
+    )
+    const headerElSearch = document.querySelector(".header-main__search")
+    const headerElFavorite = document.querySelector(".header-main__favorite")
+    const headerElTel = document.querySelector(".header-main__tell")
+
+    if (e.matches) {
+      parrentEl.append(headerElButton)
+      parrentEl.append(headerElSearch)
+      parrentEl.append(headerElFavorite)
+    } else {
+      mainContainer.append(headerElButton)
+      mainContainer.append(headerElSearch)
+      mainContainer.append(headerElTel)
+      mainContainer.append(headerElFavorite)
+    }
+  }
+  handleScreenChange767(mediaQuery767)
+
+  mediaQuery767.addEventListener("change", handleScreenChange767)
 }
